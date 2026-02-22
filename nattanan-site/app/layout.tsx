e9696b1b08200094e9c53c68def834833bb9c7d4
Nattanan Weerapong <kafaak.456@gmail.com>
2026-02-22 12:07:20 -0500

    Deploying full VMI dashboard code

---
+++ nattanan-site/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Nattanan Weerapong | Portfolio", // This changes the browser tab name 
+  description: "Mechanical Engineering Studentat VMI", // [cite: 4, 5]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
